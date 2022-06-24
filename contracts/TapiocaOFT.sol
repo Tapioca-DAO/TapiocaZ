0f8b8f9665394646e407c11f75d4b948a9be078c
0xRektora <bloodyline01@gmail.com>
2022-06-24 11:21:32 -0400

    TOFT: Updated core

--- contracts/TapiocaOFT.sol
+++ contracts/TapiocaOFT.sol
@@ -4,20 +4,51 @@ pragma solidity ^0.8.0;
 import './OFT20/OFT.sol';
 
 contract TapiocaOFT is OFT {
-    ERC20 immutable erc20;
+    ERC20 public immutable erc20;
+    uint256 public immutable mainChainID;
     uint8 _decimalCache;
 
+    uint16 constant OPTIMISM_CHAINID = 10;
+
+    // ==========
+    // * EVENTS *
+    // ==========
     event Wrap(address indexed _from, address indexed _to, uint256 _amount);
     event Unwrap(address indexed _from, address indexed _to, uint256 _amount);
 
+    // ==========
+    // * ERRORS *
+    // ==========
+    error NotMainChain();
+
     constructor(
-        string memory _name,
-        string memory _symbol,
         address _lzEndpoint,
-        ERC20 _erc20
-    ) OFT(_name, _symbol, _lzEndpoint) {
+        ERC20 _erc20,
+        uint8 _mainChainID
+    )
+        OFT(
+            string(abi.encodePacked('TapiocaWrapper-', _erc20.name())),
+            string(abi.encodePacked('TW-', _erc20.symbol())),
+            _lzEndpoint
+        )
+    {
         erc20 = _erc20;
         _decimalCache = erc20.decimals();
+        mainChainID = _mainChainID;
+
+        // Set trusted remote
+        if (getChainId() == _mainChainID) {
+            trustedRemoteLookup[OPTIMISM_CHAINID] = abi.encode(address(this));
+        } else {
+            trustedRemoteLookup[_mainChainID] = abi.encode(address(this));
+        }
+    }
+
+    modifier onlyMainChain() {
+        if (mainChainID != getChainId()) {
+            revert NotMainChain();
+        }
+        _;
     }
 
     function decimals() public view override returns (uint8) {
@@ -25,16 +56,23 @@ contract TapiocaOFT is OFT {
     }
 
     /// @notice Wrap an ERC20 with a 1:1 ratio
-    function wrap(address _toAddress, uint256 _amount) external {
+    function wrap(address _toAddress, uint256 _amount) external onlyMainChain {
         erc20.transferFrom(msg.sender, _toAddress, _amount);
         _mint(_toAddress, _amount);
         emit Wrap(msg.sender, _toAddress, _amount);
     }
 
     // @notice Unwrap an ERC20 with a 1:1 ratio
-    function unwrap(address _toAddress, uint256 _amount) external {
+    function unwrap(address _toAddress, uint256 _amount)
+        external
+        onlyMainChain
+    {
         _burn(msg.sender, _amount);
         erc20.transfer(_toAddress, _amount);
         emit Unwrap(msg.sender, _toAddress, _amount);
+    }
+
+    function getChainId() internal view virtual returns (uint256) {
+        return block.chainid;
     }
 }
