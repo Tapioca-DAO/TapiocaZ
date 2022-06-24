54cad5be5faf9ca51898299ae76968871b27de92
0xRektora <bloodyline01@gmail.com>
2022-06-24 11:21:47 -0400

    TWRP: Updated core

--- contracts/TapiocaWrapper.sol
+++ contracts/TapiocaWrapper.sol
@@ -1,19 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 import './TapiocaOFT.sol';
 
+import '@openzeppelin/contracts/utils/Create2.sol';
 import '@rari-capital/solmate/src/auth/Owned.sol';
 
 contract TapiocaWrapper is Owned {
-    TapiocaOFT[] tapiocaOFTs;
+    TapiocaOFT[] public tapiocaOFTs;
 
     constructor() Owned(msg.sender) {}
 
-    function createOFT(
-        string memory _name,
-        string memory _symbol,
-        address _lzEndpoint,
-        ERC20 _erc20
-    ) external onlyOwner {
-        tapiocaOFTs.push(new TapiocaOFT(_name, _symbol, _lzEndpoint, _erc20));
+    function tapiocaOFTLength() public view returns (uint256) {
+        return tapiocaOFTs.length;
+    }
+
+    function createTOFT(address erc20, bytes calldata bytecode)
+        external
+        onlyOwner
+    {
+        TapiocaOFT toft = TapiocaOFT(
+            Create2.deploy(
+                0,
+                keccak256(abi.encodePacked(keccak256('TapiocaWrapper'), erc20)),
+                bytecode
+            )
+        );
+        tapiocaOFTs.push(toft);
+
+        require(address(toft.erc20()) == erc20, 'ERC20 address mismatch');
+    }
+
+    function executeTOFT(address toft, bytes calldata bytecode)
+        external
+        payable
+        onlyOwner
+        returns (bool success)
+    {
+        (success, ) = payable(toft).call{value: msg.value}(bytecode);
     }
 }
