f10929ac6e823b6707a135b8932bc53e032ecd7f
0xRektora <bloodyline01@gmail.com>
2022-07-04 11:22:35 -0400

    Better task description

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -1,5 +1,6 @@
 import '@nomiclabs/hardhat-ethers';
 import { task } from 'hardhat/config';
+import { LZ_ENDPOINT } from './scripts/constants';
 import { deployTOFT } from './tasks/DeployTOFT';
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
@@ -15,5 +16,14 @@ task(
     'Deploy a TOFT off an ERC20 address on the current chain',
     deployTOFT,
 )
-    .addParam('erc20Address', 'The ERC20 address to wrap')
-    .addOptionalParam('mainChainID', 'The main chain ID ()');
+    .addOptionalParam('erc20', '(optional) The ERC20 address to wrap')
+    .addParam('chainid', `The main chain ID ()\n${formatLZEndpoints()}`);
+
+function formatLZEndpoints() {
+    return Object.keys(LZ_ENDPOINT)
+        .map((chainId) => {
+            const { name, address, lzChainId } = LZ_ENDPOINT[chainId];
+            return `${name} (${chainId}) ${address} ${lzChainId}\n`;
+        })
+        .reduce((p, c) => p + c, '');
+}
--- tasks/DeployTOFT.ts
+++ tasks/DeployTOFT.ts
@@ -5,30 +5,26 @@ import { TapiocaWrapper } from '../typechain';
 
 export const deployTOFT = async (
     args: {
-        mainChainID: number;
-        erc20Address?: string;
+        chainid: number;
+        erc20?: string;
     },
     hre: HardhatRuntimeEnvironment,
 ) => {
-    args.erc20Address = args.erc20Address
-        ? args.erc20Address
+    args.erc20 = args.erc20
+        ? args.erc20
         : '0x0000000000000000000000000000000000000000';
 
     // Verify that the address is valid
     const chainID = await hre.getChainId();
-    const erc20Name = VALID_ADDRESSES[chainID][args.erc20Address];
+    const erc20Name = VALID_ADDRESSES[chainID]?.[args.erc20];
     if (erc20Name === undefined) {
-        throw new Error(`Invalid ERC20 address: ${args.erc20Address}`);
+        throw new Error(`[-] ERC20 not whitelisted: ${args.erc20}]\n`);
     }
 
     // Get the deploy tx
     const { Tx_deployTapiocaOFT } = useUtils(hre);
     const lzEndpoint = LZ_ENDPOINT[chainID].address;
-    const tx = await Tx_deployTapiocaOFT(
-        lzEndpoint,
-        args.erc20Address,
-        args.mainChainID,
-    );
+    const tx = await Tx_deployTapiocaOFT(lzEndpoint, args.erc20, args.chainid);
 
     // Get the TWrapper
     const twrapper = (await hre.deployments.get(
@@ -36,7 +32,7 @@ export const deployTOFT = async (
     )) as unknown as TapiocaWrapper;
 
     // Create the TOFT
-    await (await twrapper.createTOFT(args.erc20Address, tx)).wait();
+    await (await twrapper.createTOFT(args.erc20, tx)).wait();
     const lastTOFT = await twrapper.lastTOFT();
 
     console.log(`Deployed ${erc20Name} TOFT at ${lastTOFT}`);
