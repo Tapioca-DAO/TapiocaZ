e81477ff2df2b7027ecd8e31e372767d0f598cdc
0xRektora <bloodyline01@gmail.com>
2022-07-05 12:12:41 -0400

    Test data

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -23,5 +23,5 @@ export const LZ_ENDPOINT: TLZ_Endpoint = {
 
 // Whitelisted addresses of an ERC20 contract, used for hh tasks
 export const VALID_ADDRESSES: any = {
-    '4': { '0xe4C519d51df6F4e62c677751512EfFf097258199': 'erc20TEST0' },
+    '4': { '0xeC1a6e378565b0568370C205fBF5CF80FE5b3F4f': 'erc20TEST0' },
 };
