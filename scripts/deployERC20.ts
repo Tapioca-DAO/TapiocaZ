dbd1948f376c9568f5ae05b2a5d5b3bb9243b23b
0xRektora <bloodyline01@gmail.com>
2022-07-07 18:00:21 -0400

    Mint test tokens

--- scripts/deployERC20.ts
+++ scripts/deployERC20.ts
@@ -2,10 +2,18 @@ import hre from 'hardhat';
 
 const deployERC20 = async () => {
     const test0 = await (
-        await hre.ethers.getContractFactory('ERC20')
+        await hre.ethers.getContractFactory('ERC20Mock')
     ).deploy('erc20TEST0', 'TEST0');
     await test0.deployed();
     console.log('\n', await hre.getChainId(), test0.address);
+    console.log('10e6 free mint for deployer ');
+
+    await test0.mint(
+        (
+            await hre.ethers.getSigners()
+        )[0].address,
+        hre.ethers.utils.parseEther((10e6).toString()),
+    );
 };
 
 deployERC20()
