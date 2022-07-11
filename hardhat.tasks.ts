de7a6c2d3bda903ab917c4178cb1e582f485445d
0xRektora <bloodyline01@gmail.com>
2022-07-11 11:57:57 -0400

    New `toftSendFrom` & `wrap` tasks

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -3,6 +3,17 @@ import { task } from 'hardhat/config';
 import { LZ_ENDPOINT } from './scripts/constants';
 import { deployTOFT } from './tasks/DeployTOFT';
 import { listDeploy } from './tasks/listDeploy';
+import { toftSendFrom } from './tasks/TOFTsendFrom';
+import { wrap } from './tasks/wrap';
+
+function formatLZEndpoints() {
+    return Object.keys(LZ_ENDPOINT)
+        .map((chainId) => {
+            const { name, address, lzChainId } = LZ_ENDPOINT[chainId];
+            return `${name} (${chainId}) ${address} ${lzChainId}\n`;
+        })
+        .reduce((p, c) => p + c, '');
+}
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
@@ -24,13 +35,17 @@ task(
     deployTOFT,
 )
     .addParam('erc20', 'The ERC20 address to wrap')
-    .addParam('chainid', `The main chain ID ()\n${formatLZEndpoints()}`);
+    .addParam('lzChainId', `The main chain ID ()\n${formatLZEndpoints()}`);
 
-function formatLZEndpoints() {
-    return Object.keys(LZ_ENDPOINT)
-        .map((chainId) => {
-            const { name, address, lzChainId } = LZ_ENDPOINT[chainId];
-            return `${name} (${chainId}) ${address} ${lzChainId}\n`;
-        })
-        .reduce((p, c) => p + c, '');
-}
+task(
+    'sendFrom',
+    'Execute a sendFrom transaction from the current account',
+    toftSendFrom,
+)
+    .addParam('toft', 'The TOFT contract')
+    .addParam('to', 'Where to send the tokens')
+    .addParam('amount', 'The amount of tokens to send');
+
+task('wrap', 'Approve and wrap an ERC20 to its TOFT', wrap)
+    .addParam('toft', 'The TOFT contract')
+    .addParam('amount', 'The amount of ERC20 to wrap');
---
+++ tasks/TOFTsendFrom.ts
@@ -0,0 +1,74 @@
+import { ParamType } from 'ethers/lib/utils';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { LZ_ENDPOINT } from '../scripts/constants';
+import { readTOFTDeployments } from '../scripts/utils';
+import { TapiocaOFT } from '../typechain';
+
+export const toftSendFrom = async (
+    args: { toft: string; to: string; amount: string },
+    hre: HardhatRuntimeEnvironment,
+) => {
+    const signer = (await hre.ethers.getSigners())[0];
+
+    const chainId = await hre.getChainId();
+    const lzChain = LZ_ENDPOINT[chainId];
+
+    const deployments = readTOFTDeployments();
+    const toft = deployments[lzChain.lzChainId].find(
+        (e) => e.address === args.toft,
+    );
+
+    if (!toft) throw new Error(`[-] TOFT not deployed on chain ${chainId}`);
+    const otherChainId = Object.keys(deployments).find((e) =>
+        deployments[e].find((o) => o.erc20address === toft?.erc20address),
+    );
+    if (!otherChainId) throw new Error('[-] TOFT not deployed on other chain');
+    const otherChainTOFT = deployments[otherChainId!].find(
+        (e) => e.erc20address === toft?.erc20address,
+    );
+    if (!otherChainTOFT)
+        throw new Error(
+            `[-] TOFT not not found on other chain ${otherChainId}`,
+        );
+
+    const toftContract = await hre.ethers.getContractAt(
+        'TapiocaOFT',
+        toft.address,
+    );
+
+    const sendFromParam: Parameters<TapiocaOFT['sendFrom']> = [
+        signer.address,
+        otherChainId,
+        args.to,
+        args.amount,
+        signer.address,
+        signer.address,
+        '0x',
+    ];
+    const payload = toftContract.interface.encodeFunctionData(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        'sendFrom',
+        sendFromParam,
+    );
+
+    const lzEndpoint = await hre.ethers.getContractAt(
+        'LZEndpointMock',
+        LZ_ENDPOINT[chainId].address,
+    );
+    const feeEstimation = await lzEndpoint.estimateFees(
+        LZ_ENDPOINT[chainId].lzChainId,
+        otherChainTOFT.address,
+        payload,
+        false,
+        '0x',
+    );
+
+    await (
+        await signer.sendTransaction({
+            to: toft.address,
+            value: feeEstimation._nativeFee,
+            data: payload,
+        })
+    ).wait();
+};
