fd65bf99a4663a3a47c58c778429d3ad82d16281
0xRektora <bloodyline01@gmail.com>
2022-07-22 13:51:42 -0400

    Refactored task

--- tasks/listDeploy.ts
+++ tasks/listDeploy.ts
@@ -1,15 +1,22 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { readTOFTDeployments } from '../scripts/utils';
+import { readTOFTDeployments, TDeployment } from '../scripts/utils';
 
-export const listDeploy = async ({}, hre: HardhatRuntimeEnvironment) => {
+export const getDeployments = async (
+    {},
+    hre: HardhatRuntimeEnvironment,
+): Promise<TDeployment> => {
     const deployments = await hre.deployments.all();
     const formatted: any = {};
     for (const key of Object.keys(deployments)) {
         formatted[key] = deployments[key].address;
     }
 
-    console.log({
+    return {
         ...(readTOFTDeployments()[await hre.getChainId()] ?? {}),
         ...formatted,
-    });
+    };
+};
+
+export const listDeploy = async ({}, hre: HardhatRuntimeEnvironment) => {
+    console.log(await getDeployments({}, hre));
 };
