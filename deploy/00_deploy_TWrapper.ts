0a3a0120ebbc3893c19e5571db7defcde37a4079
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-08-17 13:13:49 -0400

    Removed code

--- deploy/00_deploy_TWrapper.ts
+++ deploy/00_deploy_TWrapper.ts
@@ -10,17 +10,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         from: deployer,
         log: true,
     });
-
-    if (hre.network.live || hre.network.tags['rinkeby']) {
-        try {
-            const twrapper = await deployments.get('TapiocaWrapper');
-            await hre.run('verify', {
-                address: twrapper.address,
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    }
 };
 export default func;
 func.tags = ['TapiocaWrapper'];
