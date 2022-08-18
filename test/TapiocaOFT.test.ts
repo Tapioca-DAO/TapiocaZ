1e9a812914f60fb7f81be61553a9f1363cc25bec
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-08-18 14:55:21 -0400

    test: Refactored code to use fixtures

--- test/TapiocaOFT.test.ts
+++ test/TapiocaOFT.test.ts
@@ -1,160 +1,89 @@
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { BigNumberish } from 'ethers';
-import hre, { ethers } from 'hardhat';
+import { ethers } from 'hardhat';
 import { BN } from '../scripts/utils';
-import {
-    ERC20Mock,
-    LZEndpointMock,
-    TapiocaOFTMock,
-    TapiocaWrapper,
-} from '../typechain';
-import { register } from './test.utils';
+import { setupFixture } from './fixtures';
 
 describe('TapiocaOFT', () => {
-    let signer: SignerWithAddress;
-    let LZEndpointMock0: LZEndpointMock;
-    let LZEndpointMock1: LZEndpointMock;
-    let tapiocaWrapper: TapiocaWrapper;
-
-    let erc20Mock: ERC20Mock;
-
-    let tapiocaOFT0: TapiocaOFTMock;
-    let tapiocaOFT10: TapiocaOFTMock;
-
-    const amount = ethers.BigNumber.from(1e5);
-
-    beforeEach(async () => {
-        signer = (await ethers.getSigners())[0];
-
-        const {
-            LZEndpointMock0: _LZEndpointMock0,
-            LZEndpointMock1: _LZEndpointMock1,
-            tapiocaWrapper: _tapiocaWrapper,
-            utils,
-        } = await register(hre);
-        LZEndpointMock0 = _LZEndpointMock0;
-        LZEndpointMock1 = _LZEndpointMock1;
-
-        tapiocaWrapper = _tapiocaWrapper;
-        await tapiocaWrapper.setMngmtFee(25); // 0.25%
-
-        erc20Mock = await (
-            await hre.ethers.getContractFactory('ERC20Mock')
-        ).deploy('erc20Mock', 'MOCK');
-
-        // Deploy TapiocaOFT0
-        await tapiocaWrapper.createTOFT(
-            erc20Mock.address,
-            (
-                await utils.Tx_deployTapiocaOFT(
-                    LZEndpointMock0.address,
-                    erc20Mock.address,
-                    0,
-                    signer,
-                    0,
-                )
-            ).txData,
-        );
-
-        tapiocaOFT0 = (await utils.attachTapiocaOFT(
-            await tapiocaWrapper.tapiocaOFTs(
-                (await tapiocaWrapper.tapiocaOFTLength()).sub(1),
-            ),
-        )) as TapiocaOFTMock;
-
-        // Deploy TapiocaOFT10
-        await tapiocaWrapper.createTOFT(
-            erc20Mock.address,
-            (
-                await utils.Tx_deployTapiocaOFT(
-                    LZEndpointMock1.address,
-                    erc20Mock.address,
-                    0,
-                    signer,
-                    10,
-                )
-            ).txData,
-        );
-
-        tapiocaOFT10 = (await utils.attachTapiocaOFT(
-            await tapiocaWrapper.tapiocaOFTs(
-                (await tapiocaWrapper.tapiocaOFTLength()).sub(1),
-            ),
-        )) as TapiocaOFTMock;
-
-        // Link endpoints with addresses
-        LZEndpointMock0.setDestLzEndpoint(
-            tapiocaOFT10.address,
-            LZEndpointMock1.address,
-        );
-        LZEndpointMock1.setDestLzEndpoint(
-            tapiocaOFT0.address,
-            LZEndpointMock0.address,
-        );
-    });
-
-    const estimateFees = async (amount: BigNumberish) =>
-        await tapiocaOFT0.estimateFees(
-            await tapiocaWrapper.mngmtFee(),
-            await tapiocaWrapper.mngmtFeeFraction(),
-            amount,
-        );
-
-    const mintAndApprove = async (
-        erc20Mock: ERC20Mock,
-        toft: TapiocaOFTMock,
-        signer: SignerWithAddress,
-        amount: BigNumberish,
-    ) => {
-        const fees = await estimateFees(amount);
-        const amountWithFees = BN(amount).add(fees);
-
-        await erc20Mock.mint(signer.address, amountWithFees);
-        await erc20Mock.approve(toft.address, amountWithFees);
-    };
-
     it('decimals()', async () => {
+        const { erc20Mock, tapiocaOFT0, tapiocaOFT10 } = await loadFixture(
+            setupFixture,
+        );
+
         expect(await tapiocaOFT0.decimals()).eq(await erc20Mock.decimals());
         expect(await tapiocaOFT10.decimals()).eq(await erc20Mock.decimals());
     });
 
     describe('wrap()', () => {
         it('Should fail if not on the same chain', async () => {
+            const {
+                signer,
+                erc20Mock,
+                tapiocaOFT0,
+                tapiocaOFT10,
+                mintAndApprove,
+                dummyAmount,
+            } = await loadFixture(setupFixture);
+
             await expect(
-                tapiocaOFT10.wrap(signer.address, amount),
+                tapiocaOFT10.wrap(signer.address, dummyAmount),
             ).to.be.revertedWithCustomError(tapiocaOFT10, 'TOFT__NotMainChain');
 
-            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, amount);
-            await expect(tapiocaOFT0.wrap(signer.address, amount)).to.not.be
-                .reverted;
+            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, dummyAmount);
+            await expect(tapiocaOFT0.wrap(signer.address, dummyAmount)).to.not
+                .be.reverted;
         });
 
         it('Should fail if the fees are not paid', async () => {
+            const {
+                signer,
+                erc20Mock,
+                tapiocaOFT0,
+                mintAndApprove,
+                estimateFees,
+                dummyAmount,
+            } = await loadFixture(setupFixture);
             await mintAndApprove(
                 erc20Mock,
                 tapiocaOFT0,
                 signer,
-                BN(amount).sub(await estimateFees(amount)),
+                BN(dummyAmount).sub(await estimateFees(dummyAmount)),
             );
             await expect(
-                tapiocaOFT0.wrap(signer.address, amount),
+                tapiocaOFT0.wrap(signer.address, dummyAmount),
             ).to.be.revertedWith('ERC20: insufficient allowance');
         });
 
         it('Should wrap and give a 1:1 ratio amount of tokens', async () => {
-            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, amount);
-            await tapiocaOFT0.wrap(signer.address, amount);
-            expect(await tapiocaOFT0.balanceOf(signer.address)).eq(amount);
+            const {
+                signer,
+                erc20Mock,
+                tapiocaOFT0,
+                mintAndApprove,
+                dummyAmount,
+            } = await loadFixture(setupFixture);
+            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, dummyAmount);
+            await tapiocaOFT0.wrap(signer.address, dummyAmount);
+            expect(await tapiocaOFT0.balanceOf(signer.address)).eq(dummyAmount);
             expect(await erc20Mock.balanceOf(signer.address)).eq(0);
         });
     });
 
     describe('sendFrom()', () => {
         it('Should fail if untrusted remote', async () => {
+            const {
+                signer,
+                tapiocaWrapper,
+                erc20Mock,
+                tapiocaOFT0,
+                tapiocaOFT10,
+                mintAndApprove,
+                dummyAmount,
+            } = await loadFixture(setupFixture);
+
             // Setup
-            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, amount);
-            await tapiocaOFT0.wrap(signer.address, amount);
+            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, dummyAmount);
+            await tapiocaOFT0.wrap(signer.address, dummyAmount);
 
             // Failure
             await expect(
@@ -206,10 +135,19 @@ describe('TapiocaOFT', () => {
 
     describe('harvestFees', () => {
         it('Should be called only on MainChain', async () => {
-            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, amount);
-            await tapiocaOFT0.wrap(signer.address, amount);
+            const {
+                signer,
+                erc20Mock,
+                tapiocaOFT0,
+                tapiocaOFT10,
+                mintAndApprove,
+                estimateFees,
+                dummyAmount,
+            } = await loadFixture(setupFixture);
+            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, dummyAmount);
+            await tapiocaOFT0.wrap(signer.address, dummyAmount);
 
-            const fees = await estimateFees(amount);
+            const fees = await estimateFees(dummyAmount);
             expect(fees.gt(0)).to.be.true;
 
             await expect(tapiocaOFT0.harvestFees()).to.emit(
@@ -223,8 +161,16 @@ describe('TapiocaOFT', () => {
         });
 
         it('Should withdraw the fees and update the total fee balance', async () => {
-            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, amount);
-            await tapiocaOFT0.wrap(signer.address, amount);
+            const {
+                signer,
+                tapiocaWrapper,
+                erc20Mock,
+                tapiocaOFT0,
+                mintAndApprove,
+                dummyAmount,
+            } = await loadFixture(setupFixture);
+            await mintAndApprove(erc20Mock, tapiocaOFT0, signer, dummyAmount);
+            await tapiocaOFT0.wrap(signer.address, dummyAmount);
 
             const feesBefore = await tapiocaOFT0.totalFees();
 
---
+++ test/fixtures.ts
@@ -0,0 +1,108 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { BigNumberish, ethers } from 'ethers';
+import hre from 'hardhat';
+import { BN } from '../scripts/utils';
+import { ERC20Mock, TapiocaOFTMock } from '../typechain';
+import { register } from './test.utils';
+
+export const setupFixture = async () => {
+    const signer = (await hre.ethers.getSigners())[0];
+
+    const erc20Mock = await (
+        await hre.ethers.getContractFactory('ERC20Mock')
+    ).deploy('erc20Mock', 'MOCK');
+
+    const { LZEndpointMock0, LZEndpointMock1, tapiocaWrapper, utils } =
+        await register(hre);
+
+    await tapiocaWrapper.setMngmtFee(25); // 0.25%
+
+    // Deploy TapiocaOFT0
+    await tapiocaWrapper.createTOFT(
+        erc20Mock.address,
+        (
+            await utils.Tx_deployTapiocaOFT(
+                LZEndpointMock0.address,
+                erc20Mock.address,
+                0,
+                signer,
+                0,
+            )
+        ).txData,
+    );
+
+    const tapiocaOFT0 = (await utils.attachTapiocaOFT(
+        await tapiocaWrapper.tapiocaOFTs(
+            (await tapiocaWrapper.tapiocaOFTLength()).sub(1),
+        ),
+    )) as TapiocaOFTMock;
+
+    // Deploy TapiocaOFT10
+    await tapiocaWrapper.createTOFT(
+        erc20Mock.address,
+        (
+            await utils.Tx_deployTapiocaOFT(
+                LZEndpointMock1.address,
+                erc20Mock.address,
+                0,
+                signer,
+                10,
+            )
+        ).txData,
+    );
+
+    const tapiocaOFT10 = (await utils.attachTapiocaOFT(
+        await tapiocaWrapper.tapiocaOFTs(
+            (await tapiocaWrapper.tapiocaOFTLength()).sub(1),
+        ),
+    )) as TapiocaOFTMock;
+
+    // Link endpoints with addresses
+    LZEndpointMock0.setDestLzEndpoint(
+        tapiocaOFT10.address,
+        LZEndpointMock1.address,
+    );
+    LZEndpointMock1.setDestLzEndpoint(
+        tapiocaOFT0.address,
+        LZEndpointMock0.address,
+    );
+
+    const dummyAmount = ethers.BigNumber.from(1e5);
+
+    const estimateFees = async (amount: BigNumberish) =>
+        await tapiocaOFT0.estimateFees(
+            await tapiocaWrapper.mngmtFee(),
+            await tapiocaWrapper.mngmtFeeFraction(),
+            amount,
+        );
+
+    const mintAndApprove = async (
+        erc20Mock: ERC20Mock,
+        toft: TapiocaOFTMock,
+        signer: SignerWithAddress,
+        amount: BigNumberish,
+    ) => {
+        const fees = await estimateFees(amount);
+        const amountWithFees = BN(amount).add(fees);
+
+        await erc20Mock.mint(signer.address, amountWithFees);
+        await erc20Mock.approve(toft.address, amountWithFees);
+    };
+
+    const vars = {
+        signer,
+        LZEndpointMock0,
+        LZEndpointMock1,
+        tapiocaWrapper,
+        erc20Mock,
+        tapiocaOFT0,
+        tapiocaOFT10,
+        dummyAmount,
+    };
+    const functions = {
+        estimateFees,
+        mintAndApprove,
+    };
+
+    return { ...vars, ...functions };
+};
