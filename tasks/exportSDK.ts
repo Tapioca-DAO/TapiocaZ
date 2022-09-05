c39478ed79bc0e61383c9d19e2d676e733aa18ea
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-09-05 11:04:48 -0400

    refactor: Adapted to new SDK API

--- tasks/exportSDK.ts
+++ tasks/exportSDK.ts
@@ -1,52 +1,16 @@
-import fs from 'fs';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { glob, runTypeChain } from 'typechain';
-import writeJsonFile from 'write-json-file';
-import { LZ_ENDPOINTS } from '../scripts/constants';
+import { API } from 'tapioca-sdk';
+import { DEPLOYMENTS_FILE } from '../scripts/constants';
+
 /**
  * Script used to generate typings for the tapioca-sdk
  * https://github.com/Tapioca-DAO/tapioca-sdk
  */
 export const exportSDK__task = async ({}, hre: HardhatRuntimeEnvironment) => {
-    const cwd = process.cwd();
-
-    const __deployments: any = { prev: {} };
-    try {
-        __deployments.prev = JSON.parse(
-            fs.readFileSync('tapioca-sdk/src/addresses.json', 'utf-8'),
-        );
-    } catch (e) {}
-
-    const deployments = {
-        ...__deployments.prev,
-        [await hre.getChainId()]: {
-            ...__deployments.prev[await hre.getChainId()],
-            tapiocaWrapper: (await hre.deployments.get('TapiocaWrapper'))
-                .address,
-        },
-    };
-
-    await writeJsonFile('tapioca-sdk/src/addresses.json', deployments);
-    await writeJsonFile('tapioca-sdk/src/lz_endpoints.json', LZ_ENDPOINTS);
-
-    const allFiles = glob(cwd, [
-        `${hre.config.paths.artifacts}/**/!(*.dbg).json`,
-    ]).filter((e) =>
-        ['TapiocaWrapper', 'TapiocaOFT'].some(
-            (v) => e.split('/').slice(-1)[0] === v.concat('.json'),
-        ),
-    );
-
-    await runTypeChain({
-        cwd,
-        filesToProcess: allFiles,
-        allFiles,
-        outDir: 'tapioca-sdk/src/typechain/TapiocaZ',
-        target: 'ethers-v5',
-        flags: {
-            alwaysGenerateOverloads: true,
-            environment: 'hardhat',
-            discriminateTypes: true,
-        },
+    await API.exportSDK.run({
+        projectCaller: 'TapiocaZ',
+        contractNames: ['TapiocaWrapper', 'TapiocaOFT'],
+        artifactPath: hre.config.paths.artifacts,
+        _deployments: DEPLOYMENTS_FILE,
     });
 };
