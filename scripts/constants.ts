2803d5b1565899135fe609dd97e6b9550aa75456
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-09-05 11:04:23 -0400

    refactor: Cleaned SDK redundant code

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,25 +1,7 @@
-export const OPTIMISM_CHAIN_ID = 10;
+import deploymentJSON from '../deployments.json';
 
-export type TLZ_Endpoint = {
-    [chainId: string]: {
-        name: string;
-        address: string;
-        lzChainId: string;
-    };
-};
-
-export const LZ_ENDPOINTS: TLZ_Endpoint = {
-    '4': {
-        name: 'rinkeby',
-        address: '0x79a63d6d8BBD5c6dfc774dA79bCcD948EAcb53FA',
-        lzChainId: '10001',
-    },
-    '80001': {
-        name: 'mumbai',
-        address: '0xf69186dfBa60DdB133E91E9A4B5673624293d8F8',
-        lzChainId: '10009',
-    },
-};
+export const DEPLOYMENTS_PATH = 'deployments.json';
+export const DEPLOYMENTS_FILE = deploymentJSON;
 
 // Whitelisted addresses of ERC20 contracts. Per LZ chain id.
 export const VALID_ADDRESSES: any = {
