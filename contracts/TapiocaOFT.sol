9ad9cdb56cec63b3942847e4145eef5d41b3c385
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-11-16 12:15:33 -0500

    feat/fix: Support natives - New `isNative` state var - New `TOFT_ZeroAmount` error - New `wrappedAmount()` view - New `wrapNative()` - `unwrap()` adaptation

--- contracts/TapiocaOFT.sol
+++ contracts/TapiocaOFT.sol
@@ -2,10 +2,31 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import 'tapioca-sdk/dist/contracts/lzApp/LzApp.sol';
 import 'tapioca-sdk/dist/contracts/token/oft/OFT.sol';
 import 'tapioca-sdk/dist/contracts/interfaces/ILayerZeroEndpoint.sol';
 import './TapiocaWrapper.sol';
+
+//
+//                 .(%%%%%%%%%%%%*       *
+//             #%%%%%%%%%%%%%%%%%%%%*  ####*
+//          #%%%%%%%%%%%%%%%%%%%%%#  /####
+//       ,%%%%%%%%%%%%%%%%%%%%%%%   ####.  %
+//                                #####
+//                              #####
+//   #####%#####              *####*  ####%#####*
+//  (#########(              #####     ##########.
+//  ##########             #####.      .##########
+//                       ,####/
+//                      #####
+//  %%%%%%%%%%        (####.           *%%%%%%%%%#
+//  .%%%%%%%%%%     *####(            .%%%%%%%%%%
+//   *%%%%%%%%%%   #####             #%%%%%%%%%%
+//               (####.
+//      ,((((  ,####(          /(((((((((((((
+//        *,  #####  ,(((((((((((((((((((((
+//          (####   ((((((((((((((((((((/
+//         ####*  (((((((((((((((((((
+//                     ,**//*,.
 
 contract TapiocaOFT is OFT {
     using SafeERC20 for IERC20;
@@ -18,6 +39,8 @@ contract TapiocaOFT is OFT {
     IERC20 public immutable erc20;
     /// @notice The host chain ID of the ERC20, will be used only on OP chain.
     uint256 public immutable hostChainID;
+    /// @notice If this wrapper is for an ERC20 or a native token.
+    bool isNative;
     /// @notice Decimal cache number of the ERC20.
     uint8 _decimalCache;
 
@@ -27,6 +50,8 @@ contract TapiocaOFT is OFT {
 
     /// @notice Code executed not on main chain (optimism/chainID mismatch).
     error TOFT__NotHostChain();
+    /// @notice A zero amount was found
+    error TOFT_ZeroAmount();
 
     /// ==========================
     /// ========== Events ========
@@ -37,6 +62,7 @@ contract TapiocaOFT is OFT {
 
     constructor(
         address _lzEndpoint,
+        bool _isNative,
         IERC20 _erc20,
         string memory _name,
         string memory _symbol,
@@ -49,7 +75,12 @@ contract TapiocaOFT is OFT {
             _lzEndpoint
         )
     {
+        if (isNative) {
+            require(address(_erc20) == address(0), 'TOFT__NotNative');
+        }
+
         erc20 = _erc20;
+        isNative = _isNative;
         _decimalCache = _decimal;
         hostChainID = _hostChainID;
 
@@ -67,6 +98,17 @@ contract TapiocaOFT is OFT {
     /// @notice Decimal number of the ERC20
     function decimals() public view override returns (uint8) {
         return _decimalCache;
+    }
+
+    /// @notice Return the output amount of an ERC20 token wrap operation.
+    function wrappedAmount(uint256 _amount) public view returns (uint256) {
+        return
+            _amount -
+            estimateFees(
+                tapiocaWrapper.mngmtFee(),
+                tapiocaWrapper.mngmtFeeFraction(),
+                _amount
+            );
     }
 
     /// @notice Wrap an ERC20 with a 1:1 ratio with a fee if existing.
@@ -97,25 +139,60 @@ contract TapiocaOFT is OFT {
         emit Wrap(msg.sender, _toAddress, _amount);
     }
 
-    /// @notice Harvest the fees collected by the contract. Called only on main chain.
+    /// @notice Wrap a native token with a 1:1 ratio with a fee if existing.
+    /// @dev Since it can be executed only on the host chain, if an address exists on the linked chain it will not allowed to wrap.
+    /// @param _toAddress The address to wrap the tokens to.
+    function wrapNative(address _toAddress) external payable onlyHostChain {
+        if (msg.value == 0) {
+            revert TOFT_ZeroAmount();
+        }
+
+        uint256 toMint;
+        uint256 mngmtFee = tapiocaWrapper.mngmtFee();
+
+        if (mngmtFee > 0) {
+            uint256 feeAmount = estimateFees(
+                mngmtFee,
+                tapiocaWrapper.mngmtFeeFraction(),
+                msg.value
+            );
+
+            totalFees += feeAmount;
+            toMint = msg.value - feeAmount;
+        }
+
+        _mint(_toAddress, toMint);
+        emit Wrap(msg.sender, _toAddress, toMint);
+    }
+
+    /// @notice Harvest the fees collected by the contract. Called only on host chain.
     function harvestFees() external onlyHostChain {
         erc20.safeTransfer(address(tapiocaWrapper.owner()), totalFees);
         totalFees = 0;
         emit Harvest(totalFees);
     }
 
-    /// @notice Unwrap an ERC20 with a 1:1 ratio. Called only on main chain.
-    /// @param _toAddress The address to unwrap the ERC20 to.
-    /// @param _amount The amount of ERC20 to unwrap.
+    /// @notice Unwrap an ERC20/Native with a 1:1 ratio. Called only on host chain.
+    /// @param _toAddress The address to unwrap the tokens to.
+    /// @param _amount The amount of tokens to unwrap.
     function unwrap(address _toAddress, uint256 _amount)
         external
         onlyHostChain
     {
         _burn(msg.sender, _amount);
-        erc20.safeTransfer(_toAddress, _amount);
+
+        if (isNative) {
+            safeTransferETH(_toAddress, _amount);
+        } else {
+            erc20.safeTransfer(_toAddress, _amount);
+        }
+
         emit Unwrap(msg.sender, _toAddress, _amount);
     }
 
+    // ================================
+    // ========== INTERNAL ============
+    // ================================
     /// @notice Estimate the management fees for a wrap operation.
     function estimateFees(
         uint256 _feeBps,
@@ -125,7 +202,23 @@ contract TapiocaOFT is OFT {
         return (_amount * _feeBps) / _feeFraction;
     }
 
-    /// @notice Check if the current chain is the main chain of the ERC20.
+    /// @notice Author: Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/SafeTransferLib.sol)
+    function safeTransferETH(address to, uint256 amount) internal {
+        bool success;
+
+        assembly {
+            // Transfer the ETH and store if it succeeded or not.
+            success := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+
+        require(success, 'ETH_TRANSFER_FAILED');
+    }
+
+    // ==========================
+    // ========== LZ ============
+    // ==========================
+
+    /// @notice Check if the current chain is the host chain of the ERC20.
     function isHostChain() external view returns (bool) {
         return getChainId() == hostChainID;
     }
