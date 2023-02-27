761dce44558cf88937b6658c028d21971ee8814a
cryptotechmaker <grigore.cosmin@gmail.com>
2023-02-27 09:33:27 +0200

    Some fixes for task configuration

--- tasks/setTrustedRemote.ts
+++ tasks/setTrustedRemote.ts
@@ -7,43 +7,61 @@ import writeJsonFile from 'write-json-file';
 //10109-mumbai
 //10143-arb_goerli
 
-//  tAVAX
-//npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0x28D691380D2d8C86f6fdD2e49123C1DA9fa33b32 --src 0xef0871E0e8C3320f5Cf8c0051EC856b9c083660f
-//npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0x74FC744146cb0067AC34DF10c6e7bcc050439D37 --src 0xef0871E0e8C3320f5Cf8c0051EC856b9c083660f
+//arb
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0x4172056FDC344b8Fd38bfDe590a7eDdF32cD1d55 --src 0xc0106C090153F651c5E6e12249412b9e51f8d49d
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0x71dDd5ec9815740529D62726Adc50EB84a3A4e1a --src 0xc0106C090153F651c5E6e12249412b9e51f8d49d
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10112 --dst 0x5Ba1CF78AAEA752BEC33c2036B1E315C881d8E49 --src 0xc0106C090153F651c5E6e12249412b9e51f8d49d
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0x628570D3768e7424dd7Ca4671846D1b67c82E141 --src 0xd429a8F683Aa8D43Aa3CBdDCa93956CBc44c4242
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0xa1BD6C0B6b35209B3710cA6Ab306736e06C1fe9c --src 0xd429a8F683Aa8D43Aa3CBdDCa93956CBc44c4242
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10112 --dst 0x5916f519DFB4b80a3aaD07E0530b93605c35C636 --src 0xd429a8F683Aa8D43Aa3CBdDCa93956CBc44c4242
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0x05C0a8C53BED62edf009b8B870fAC065B4cc3533 --src 0xd37E276907e76bF25eBaDA04fB2dCe67c8BE5188
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0x556029CB9c74B07bC34abED41eaA424159463E50 --src 0xd37E276907e76bF25eBaDA04fB2dCe67c8BE5188
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10112 --dst 0x177b341C0E1b36f9D4fAC0F90B1ebF3a20480834 --src 0xd37E276907e76bF25eBaDA04fB2dCe67c8BE5188
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10112 --dst 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0 --src 0x4ba186b07cf3C5C4e2aB967d0Daa996dc83Ce30E
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0x8688820A09b5796840c4570747E7E0064B87d3DF --src 0x4ba186b07cf3C5C4e2aB967d0Daa996dc83Ce30E
+//  npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0x33e1eFe92dBca2d45fe131ab3a1613A169696924 --src 0x4ba186b07cf3C5C4e2aB967d0Daa996dc83Ce30E
 
-//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0xef0871E0e8C3320f5Cf8c0051EC856b9c083660f --src 0x28D691380D2d8C86f6fdD2e49123C1DA9fa33b32
-//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0x74FC744146cb0067AC34DF10c6e7bcc050439D37 --src 0x28D691380D2d8C86f6fdD2e49123C1DA9fa33b32
-
-//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0xef0871E0e8C3320f5Cf8c0051EC856b9c083660f --src 0x74FC744146cb0067AC34DF10c6e7bcc050439D37
-//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0x28D691380D2d8C86f6fdD2e49123C1DA9fa33b32 --src 0x74FC744146cb0067AC34DF10c6e7bcc050439D37
-//---
-
-//  tWETH
-//npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0x4ee2C3e02D9c47951a6a56bE803030D70F3dbfb7 --src 0xd5d5d2fed1eCb5Dea28Fe81fB575c9C241448D71
-//npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0xAa7e77fb38C8B5df58cba3a49227dAb6ee5f18Cb --src 0xd5d5d2fed1eCb5Dea28Fe81fB575c9C241448D71
-
-//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0xd5d5d2fed1eCb5Dea28Fe81fB575c9C241448D71 --src 0x4ee2C3e02D9c47951a6a56bE803030D70F3dbfb7
-//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0xAa7e77fb38C8B5df58cba3a49227dAb6ee5f18Cb --src 0x4ee2C3e02D9c47951a6a56bE803030D70F3dbfb7
-
-//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0xd5d5d2fed1eCb5Dea28Fe81fB575c9C241448D71 --src 0xAa7e77fb38C8B5df58cba3a49227dAb6ee5f18Cb
-//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0x4ee2C3e02D9c47951a6a56bE803030D70F3dbfb7 --src 0xAa7e77fb38C8B5df58cba3a49227dAb6ee5f18Cb
-//---
-
-//  tMatic
-//npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10106 --dst 0xe82f613C2B46D3fD51bA2A6Bc04a4dB65413b2a1 --src 0x48d95D182D33990910DC39868Da6FcA59182626c
-//npx hardhat setTrustedRemote --network arbitrum_goerli --chain 10109 --dst 0x18BC2Be450e04EBB72A150dfa9a268F60302215c --src 0x48d95D182D33990910DC39868Da6FcA59182626c
-
-//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0x48d95D182D33990910DC39868Da6FcA59182626c --src 0xe82f613C2B46D3fD51bA2A6Bc04a4dB65413b2a1
-//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0x18BC2Be450e04EBB72A150dfa9a268F60302215c --src 0xe82f613C2B46D3fD51bA2A6Bc04a4dB65413b2a1
-
-//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0x48d95D182D33990910DC39868Da6FcA59182626c --src 0x18BC2Be450e04EBB72A150dfa9a268F60302215c
-//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0xe82f613C2B46D3fD51bA2A6Bc04a4dB65413b2a1 --src 0x18BC2Be450e04EBB72A150dfa9a268F60302215c
-//---
-
-// tFtm -todo
+//fuji
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0xd37E276907e76bF25eBaDA04fB2dCe67c8BE5188 --src 0x05C0a8C53BED62edf009b8B870fAC065B4cc3533
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0x556029CB9c74B07bC34abED41eaA424159463E50 --src 0x05C0a8C53BED62edf009b8B870fAC065B4cc3533
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10112 --dst 0x177b341C0E1b36f9D4fAC0F90B1ebF3a20480834 --src 0x05C0a8C53BED62edf009b8B870fAC065B4cc3533
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0xc0106C090153F651c5E6e12249412b9e51f8d49d --src 0x71dDd5ec9815740529D62726Adc50EB84a3A4e1a
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0x4172056FDC344b8Fd38bfDe590a7eDdF32cD1d55 --src 0x71dDd5ec9815740529D62726Adc50EB84a3A4e1a
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10112 --dst 0x5Ba1CF78AAEA752BEC33c2036B1E315C881d8E49 --src 0x71dDd5ec9815740529D62726Adc50EB84a3A4e1a
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0xd429a8F683Aa8D43Aa3CBdDCa93956CBc44c4242 --src 0x628570D3768e7424dd7Ca4671846D1b67c82E141
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0xa1BD6C0B6b35209B3710cA6Ab306736e06C1fe9c --src 0x628570D3768e7424dd7Ca4671846D1b67c82E141
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10112 --dst 0x5916f519DFB4b80a3aaD07E0530b93605c35C636 --src 0x628570D3768e7424dd7Ca4671846D1b67c82E141
 //npx hardhat setTrustedRemote --network fuji_avalanche --chain 10112 --dst 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0 --src 0x33e1eFe92dBca2d45fe131ab3a1613A169696924
-//npx hardhat setTrustedRemote --network fantom_testnet --chain 10106 --dst 0x33e1eFe92dBca2d45fe131ab3a1613A169696924 --src 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10143 --dst 0x4ba186b07cf3C5C4e2aB967d0Daa996dc83Ce30E --src 0x33e1eFe92dBca2d45fe131ab3a1613A169696924
+//npx hardhat setTrustedRemote --network fuji_avalanche --chain 10109 --dst 0x8688820A09b5796840c4570747E7E0064B87d3DF --src 0x33e1eFe92dBca2d45fe131ab3a1613A169696924
 
+//mumbai
+//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0xd429a8F683Aa8D43Aa3CBdDCa93956CBc44c4242 --src 0xa1BD6C0B6b35209B3710cA6Ab306736e06C1fe9c
+//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0x628570D3768e7424dd7Ca4671846D1b67c82E141 --src 0xa1BD6C0B6b35209B3710cA6Ab306736e06C1fe9c
+//npx hardhat setTrustedRemote --network mumbai --chain 10112 --dst 0x5916f519DFB4b80a3aaD07E0530b93605c35C636 --src 0xa1BD6C0B6b35209B3710cA6Ab306736e06C1fe9c
+//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0xc0106C090153F651c5E6e12249412b9e51f8d49d --src 0x4172056FDC344b8Fd38bfDe590a7eDdF32cD1d55
+//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0x71dDd5ec9815740529D62726Adc50EB84a3A4e1a --src 0x4172056FDC344b8Fd38bfDe590a7eDdF32cD1d55
+//npx hardhat setTrustedRemote --network mumbai --chain 10112 --dst 0x5Ba1CF78AAEA752BEC33c2036B1E315C881d8E49 --src 0x4172056FDC344b8Fd38bfDe590a7eDdF32cD1d55
+//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0xd37E276907e76bF25eBaDA04fB2dCe67c8BE5188 --src 0x556029CB9c74B07bC34abED41eaA424159463E50
+//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0x05C0a8C53BED62edf009b8B870fAC065B4cc3533 --src 0x556029CB9c74B07bC34abED41eaA424159463E50
+//npx hardhat setTrustedRemote --network mumbai --chain 10112 --dst 0x177b341C0E1b36f9D4fAC0F90B1ebF3a20480834 --src 0x556029CB9c74B07bC34abED41eaA424159463E50
+//npx hardhat setTrustedRemote --network mumbai --chain 10112 --dst 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0 --src 0x8688820A09b5796840c4570747E7E0064B87d3DF
+//npx hardhat setTrustedRemote --network mumbai --chain 10106 --dst 0x33e1eFe92dBca2d45fe131ab3a1613A169696924 --src 0x8688820A09b5796840c4570747E7E0064B87d3DF
+//npx hardhat setTrustedRemote --network mumbai --chain 10143 --dst 0x4ba186b07cf3C5C4e2aB967d0Daa996dc83Ce30E --src 0x8688820A09b5796840c4570747E7E0064B87d3DF
+
+//fantom
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10143 --dst 0x4ba186b07cf3C5C4e2aB967d0Daa996dc83Ce30E --src 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10106 --dst 0x33e1eFe92dBca2d45fe131ab3a1613A169696924 --src 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10109 --dst 0x8688820A09b5796840c4570747E7E0064B87d3DF --src 0x9C574C71eCabc7aEf19593A595fb9f8Aa6a78bB0
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10143 --dst 0xc0106C090153F651c5E6e12249412b9e51f8d49d --src 0x5Ba1CF78AAEA752BEC33c2036B1E315C881d8E49
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10106 --dst 0x71dDd5ec9815740529D62726Adc50EB84a3A4e1a --src 0x5Ba1CF78AAEA752BEC33c2036B1E315C881d8E49
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10109 --dst 0x4172056FDC344b8Fd38bfDe590a7eDdF32cD1d55 --src 0x5Ba1CF78AAEA752BEC33c2036B1E315C881d8E49
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10106 --dst 0x05C0a8C53BED62edf009b8B870fAC065B4cc3533 --src 0x177b341C0E1b36f9D4fAC0F90B1ebF3a20480834
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10109 --dst 0x556029CB9c74B07bC34abED41eaA424159463E50 --src 0x177b341C0E1b36f9D4fAC0F90B1ebF3a20480834
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10143 --dst 0xd37E276907e76bF25eBaDA04fB2dCe67c8BE5188 --src 0x177b341C0E1b36f9D4fAC0F90B1ebF3a20480834
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10109 --dst 0xa1BD6C0B6b35209B3710cA6Ab306736e06C1fe9c --src 0x5916f519DFB4b80a3aaD07E0530b93605c35C636
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10106 --dst 0x628570D3768e7424dd7Ca4671846D1b67c82E141 --src 0x5916f519DFB4b80a3aaD07E0530b93605c35C636
+//npx hardhat setTrustedRemote --network fantom_testnet --chain 10143 --dst 0xd429a8F683Aa8D43Aa3CBdDCa93956CBc44c4242 --src 0x5916f519DFB4b80a3aaD07E0530b93605c35C636
 export const setTrustedRemote__task = async (
     taskArgs: { chain: string; dst: string; src: string },
     hre: HardhatRuntimeEnvironment,
