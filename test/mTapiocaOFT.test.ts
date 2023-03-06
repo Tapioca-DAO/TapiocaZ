375ffcbe21d8e53f1a4152a9542e97780efbd99c
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-06 11:36:51 -0500

    remove `.only` test

--- test/mTapiocaOFT.test.ts
+++ test/mTapiocaOFT.test.ts
@@ -148,7 +148,7 @@ describe('mTapiocaOFT', () => {
         });
     });
 
-    it.only('Should be able to use permit', async () => {
+    it('Should be able to use permit', async () => {
         const { signer, randomUser, users, mtapiocaOFT0, mintAndApprove, mErc20Mock } = await loadFixture(setupFixture);
         const [usurper] = users;
 
