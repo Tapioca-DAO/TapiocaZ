24c35d4f571a0126c17767775058f35276f0f386
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-15 11:05:30 -0400

    patch: Adapted & added new exports

--- tasks/exportSDK.ts
+++ tasks/exportSDK.ts
@@ -1,19 +1,39 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { API } from 'tapioca-sdk';
+import { TLocalDeployment } from 'tapioca-sdk/dist/shared';
 import { DEPLOYMENTS_FILE } from '../constants';
 
 /**
  * Script used to generate typings for the tapioca-sdk
  * https://github.com/Tapioca-DAO/tapioca-sdk
  */
-export const exportSDK__task = async ({}, hre: HardhatRuntimeEnvironment) => {
-    await API.exportSDK.run({
-        projectCaller: 'TapiocaZ',
-        contractNames: ['TapiocaWrapper', 'TapiocaOFT'],
+export const exportSDK__task = async (
+    taskArgs: { tag?: string },
+    hre: HardhatRuntimeEnvironment,
+) => {
+    const tag = taskArgs.tag || 'default';
+
+    const data = hre.SDK.db.readDeployment('local', {
+        tag,
+    }) as TLocalDeployment;
+
+    const contractNames = [
+        'TapiocaWrapper',
+        'mTapiocaOFT',
+        'TapiocaOFT',
+        'Rebalancer',
+        'BaseTOFT',
+    ];
+
+    console.log(
+        '[+] Exporting typechain & deployment files for tapioca-sdk...',
+    );
+    console.log(contractNames);
+
+    hre.SDK.exportSDK.run({
+        projectCaller: hre.config.SDK.project,
         artifactPath: hre.config.paths.artifacts,
-        _deployments:
-            Object.keys(DEPLOYMENTS_FILE).length > 0
-                ? DEPLOYMENTS_FILE
-                : undefined,
+        deployment: { data, tag },
+        contractNames,
     });
 };
