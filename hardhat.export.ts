0716ae1355d3e42bf10ec46bcb9d23264bc254ac
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-12-21 12:02:09 -0500

    chore: Faster compile time

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -101,7 +101,7 @@ const config: HardhatUserConfig & { dodoc?: any; typechain?: any } = {
         enabled: false,
     },
     dodoc: {
-        runOnCompile: true,
+        runOnCompile: false,
     },
     mocha: {
         timeout: 4000000,
