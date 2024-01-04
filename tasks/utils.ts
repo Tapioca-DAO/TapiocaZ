ce6e2e4a4f1a4f0484f64658e265953ad438bd1b
cryptotechmaker <grigore.cosmin@gmail.com>
2024-01-04 18:36:27 +0200

    tofts

--- tasks/utils.ts
+++ tasks/utils.ts
@@ -7,17 +7,6 @@ export const loadVM = async (
     tag: string,
     debugMode = false,
 ) => {
-    const signer = (await hre.ethers.getSigners())[0];
-    const chainInfo = hre.SDK.utils.getChainBy(
-        'chainId',
-        await hre.getChainId(),
-    );
-    const multicallAddress =
-        hre.SDK.config.MULTICALL_ADDRESSES[chainInfo?.chainId];
-    if (!multicallAddress) {
-        throw '[-] Multicall not deployed';
-    }
-
     const VM = new hre.SDK.DeployerVM(hre, {
         // Change this if you get bytecode size error / gas required exceeds allowance (550000000)/ anything related to bytecode size
         // Could be different by network/RPC provider
