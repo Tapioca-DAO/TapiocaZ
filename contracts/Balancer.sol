24e2c34ad14b94672f53792487c10b19aef5e5ee
cryptotechmaker <grigore.cosmin@gmail.com>
2024-02-13 11:48:57 +0200

    chore: minor import fix

--- contracts/Balancer.sol
+++ contracts/Balancer.sol
@@ -2,15 +2,15 @@
 pragma solidity 0.8.22;
 
 // External
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 // Tapioca
-import "tapioca-periph/interfaces/external/stargate/IStargateEthVault.sol";
-import "tapioca-periph/interfaces/external/stargate/IStargateRouter.sol";
-import "tapioca-periph/interfaces/tapiocaz/ITOFTVault.sol";
-import "tapioca-periph/interfaces/oft/ITOFT.sol";
+import {IStargateEthVault} from "tapioca-periph/interfaces/external/stargate/IStargateEthVault.sol";
+import {IStargateRouter} from "tapioca-periph/interfaces/external/stargate/IStargateRouter.sol";
+import {ITOFTVault} from "tapioca-periph/interfaces/tapiocaz/ITOFTVault.sol";
+import {ITOFT} from "tapioca-periph/interfaces/oft/ITOFT.sol";
 
 /*
 
