7f1bfebda7ab844237477b9e880eae364a592e77
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-02-20 11:00:44 -0500

    chore(`TOFTReceiver`): Cleaned imports

--- contracts/tOFT/modules/TOFTReceiver.sol
+++ contracts/tOFT/modules/TOFTReceiver.sol
@@ -2,18 +2,11 @@
 pragma solidity 0.8.22;
 
 // LZ
-import {OFTCore} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFTCore.sol";
-import {Origin} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
 import {OFT} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
 
-// External
-import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
 // Tapioca
-import {TapiocaOmnichainReceiver} from "tapioca-periph/tapiocaOmnichainEngine/TapiocaOmnichainReceiver.sol";
 import {TOFTInitStruct} from "tapioca-periph/interfaces/oft/ITOFT.sol";
 import {BaseTOFTReceiver} from "./BaseTOFTReceiver.sol";
-
 
 /*
 
@@ -29,13 +22,7 @@ import {BaseTOFTReceiver} from "./BaseTOFTReceiver.sol";
 contract TOFTReceiver is BaseTOFTReceiver {
     constructor(TOFTInitStruct memory _data) BaseTOFTReceiver(_data) {}
 
-    function _toftCustomComposeReceiver(uint16, address, bytes memory)
-        internal
-        pure
-        override
-        returns (bool success)
-    {
+    function _toftCustomComposeReceiver(uint16, address, bytes memory) internal pure override returns (bool success) {
         return false;
     }
-    
 }
