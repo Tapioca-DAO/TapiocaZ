c613ac2a7e48e06c32b6990cdd02b211d65ee1dd
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-05-31 16:42:36 -0400

    feat(`deployment`): Add new `deployFinal__task`

--- tasks/scopes/deployScope.ts
+++ tasks/scopes/deployScope.ts
@@ -2,6 +2,7 @@ import '@nomiclabs/hardhat-ethers';
 import { scope } from 'hardhat/config';
 import { TAP_TASK } from 'tapioca-sdk';
 import { deployPostLbp__task } from 'tasks/deploy/1-deployPostLbp__task';
+import { deployFinal__task } from 'tasks/deploy/2-deployFinal__task';
 
 const deployScope = scope('deploys', 'Deployment tasks');
 
@@ -11,4 +12,18 @@ TAP_TASK(
         'Will deploy Balancer, mtETH, tWSTETH, and tRETH.',
         deployPostLbp__task,
     ),
+);
+
+TAP_TASK(
+    deployScope
+        .task(
+            'final',
+            'Will deploy SGL sDAI market OFT on Arb + Eth and link them.',
+            deployFinal__task,
+        )
+        .addParam(
+            'sDaiMarketChainName',
+            'Host chain name of the SGL sDai market.',
+            'ethereum',
+        ),
 );
